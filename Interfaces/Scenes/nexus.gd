85b146c008ebc7ade0cf9c2e087bc77367e7b517
Eero Laine <horridparlormaster@gmail.com>
2025-02-22 03:26:29 +0200

    Change theme and background color when game cleared

--- Interfaces/Scenes/nexus.gd
+++ Interfaces/Scenes/nexus.gd
@@ -6,6 +6,7 @@ signal level_selected(level_index);
 const LEVEL_BUTTON_PATH : String = "res://Prefabs/Nexus/level_button.tscn";
 const LEVEL_BUTTON_X : int = 360;
 const LEVEL_BUTTON_Y : int = 320;
+const VICTORY_THEME_PATH : String = "res://Assets/Music/VictoryTheme.mp3";
 
 var levels_unlocked : int;
 
--- Scripts/Scenes/nexus.gd
+++ Scripts/Scenes/nexus.gd
@@ -2,6 +2,8 @@ extends Nexus
 
 @onready var title_layer : GlowNode = $TitleLayer;
 @onready var buttons_layer : Node2D = $ButtonsLayer
+@onready var audio_stream : AudioStreamPlayer = $AudioStreamPlayer;
+@onready var victory_panel : Panel = $VictoryPanel;
 
 func _ready() -> void:
 	title_layer.activate_animations();
@@ -13,6 +15,8 @@ func spawn_level_buttons(levels_unlocked_ : int):
 	var level_button : LevelButton;
 	var current_position : Vector2 = Vector2(-2 * LEVEL_BUTTON_X, 0);
 	levels_unlocked = levels_unlocked_;
+	if levels_unlocked == GameplayEnums.LAST_LEVEL:
+		change_to_victory_nexus();
 	for i in range(10):
 		level_button = System.Instance.load_child(LEVEL_BUTTON_PATH, buttons_layer);
 		level_button.set_level(i);
@@ -23,6 +27,11 @@ func spawn_level_buttons(levels_unlocked_ : int):
 		if i > levels_unlocked:
 			level_button.deactivate();
 		level_button.selected.connect(on_level_selected);
+
+func change_to_victory_nexus() -> void:
+	audio_stream.stream = load(VICTORY_THEME_PATH);
+	audio_stream.play();
+	victory_panel.visible = true;
 
 func on_level_selected(level_index : int):
 	emit_signal("level_selected", level_index);
